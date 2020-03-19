d1e4c3829bc0b04ee07afac9d121596b3d3d9604
ismacm <ismail.cherkaouimestari@iesjuandeherrera.net>
2020-03-19 12:31:49 +0100

    Ejercicio clases wrapper

---
+++ Soluciones/Prog/ICHERKAOUIME/Wrapper Class/Arithmos/src/Exceptions/SingularityException.java
@@ -0,0 +1,12 @@
+package Exceptions;
+
+public class SingularityException extends Exception{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    public SingularityException(String msg) {
+        super(msg);
+    }
+}
