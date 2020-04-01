913ffdcdcf5344ec5bb3d78787eebe0ebd80957f
MarielysLopezDAW <marielysjennyre.lopezcastro@iesjuandeherrera.net>
2020-04-01 21:50:52 +0200

    Agregando un poco de javaDoc

--- Soluciones/Prog/MLOPEZCA/EjercicioWrap/BusquedaBinaria.java
+++ Soluciones/Prog/MLOPEZCA/EjercicioWrap/BusquedaBinaria.java
@@ -1,57 +1,101 @@
 package EjercicioWrap;
+import java.util.Scanner;
+// TODO: Auto-generated Javadoc
+
+/**
+ * The Class BusquedaBinaria.
+ */
 public class BusquedaBinaria {
+	//Bienvenido al programa de ordenamiento burbuja y busqueda binaria
+	//Usaremos esta clase de ejemplo para trabajar con Clases Wrappers
+	//En esta ocasión cambiaremos todos los primitivos int por clases Interger
+	
+	/** The arreglo V. */
+	private static Integer[] arregloV = new Integer [20];          //lo primero que hago es declarar un arreglo a nivel de clase para usarlo en las tres funciones
 
-	public static int[] arregloV = new int [20];          //lo primero que hago es declarar un arreglo a nivel de clase para usarlo en las dos funciones
-
-	static void ordenamientoBurbuja() {                     //en esta funcion relleno el arreglo con numeros aleatorios lo imprimo en pantalla, lo ordeno y lo imprimo ordenado
-		System.out.println("Numeros aleatorios=");
-		for(int i = 0; i<arregloV.length; i++) {            //con este for introduzco datos aleatorios en cada posicion del array
-			arregloV[i]= (int)Math.floor(Math.random()*100);//para que sean aleatorios uso el metodo Math.random y para que sean numeros del 0 al 100
-			System.out.print("["+arregloV[i]+"]");
-		}
-
-		int apuntadorDos = 1;                                //los el algoritmo de busqueda burbuja necesita dos apuntadores el primero está creado en el for externo y este es para el if que está en el for interno
-		int auxiliar = 0;                                    //aquí alojaremos la el dato que vamos a intercambiar de posición 
-
-		for(int i = 0; i < arregloV.length; i++) {                                   // con el for externo nos encargamos de dar tantas vueltas como posiciones tenga el array para así asegurarnos de comparar todas las posiciones y trasladarlas ha su lugar
-
-			for(int apuntadorUno = 0; apuntadorUno < arregloV.length; apuntadorUno++) {// con el for interno vamos a dar vueltas comparando las posiciones secuencialmente
-
-				if(apuntadorDos < arregloV.length) {								//para compararlas vamos a usar un if 
-
-					if(arregloV[apuntadorUno] > arregloV[apuntadorDos]) {
-
-						auxiliar = arregloV[apuntadorUno];							//y si se cumple la condicion alojaremos el valor más grande en una variable auxiliar
-
-						arregloV[apuntadorUno] = arregloV[apuntadorDos];				//luego el valor más pequeño se aloja donde estaba el más grande 
-
-						arregloV[apuntadorDos] = auxiliar;								//y el valor grande se aloja donde estaba el pequeño
-					}
-
-					apuntadorDos++;														//el apuntador que fue declarado externamente debe ir recorriendo más posiciones
-				}
-
-			}
-			apuntadorDos = 1;															//el apuntador dos debe volver a su posición inicial luego de cada vuelta del primer for
-		}
-		System.out.println(" ");															//esto es solo algo estetico para presentar los datos de manera ordenada
-		System.out.println(" ");
-		System.out.println("Numeros ordenados=");
-		for(int i = 0; i<arregloV.length; i++) {												//aquí imprimimos el arreglo ya ordenado
-			System.out.print("["+arregloV[i]+"]");
-		}
-		System.out.println(" ");
-		System.out.println(" ");
-
-
+	/** The valor buscado. */
+	private static Integer valorBuscado;
+	
+	/**
+	 * Instantiates a new busqueda binaria.
+	 */
+	public BusquedaBinaria() {
+		valorBuscado=(int)Math.floor(Math.random()*100);
 	}
-
-	static int busquedaBinaria(int valorBuscado) throws BusquedaException {			// en esta funcion podremos buscar un valor que entra por parametro y devolvemos la posicion donde está
-																					// al igual que el algoritmo burbuja necesitamos varios apuntadores en este caso tres apuntadores
-		int apuntador1 = 0;															//el primero estará en el inicio
-		int apuntador2 = arregloV.length;											//el segundo en el final del array
-		int apuntadorPM = 0;														//el tercero en la posicion de la mitad entre el apuntador1 y apuntador2
-		int valorEncontrado= -1;													//le doy un valor negativo porque no existe la posicion del array -1 y así puedo saber si no se ha encontrado el valor
+	
+	/**
+	 * Instantiates a new busqueda binaria.
+	 *
+	 * @param valorBuscado the valor buscado
+	 */
+	public BusquedaBinaria(Integer valorBuscado) {
+		this.valorBuscado=valorBuscado;
+	}
+	
+	/**
+	 * Arreglo original.
+	 *
+	 * @return the integer[]
+	 */
+	private static Integer [] arregloOriginal() {
+		
+		for(int i = 0; i<arregloV.length; i++) {            
+			arregloV[i]= (int)Math.floor(Math.random()*100);//para que sean aleatorios uso el metodo Math.random y para que sean numeros del 0 al 100
+		}
+		
+		return arregloV;
+	}
+	
+	/**
+	 * Algoritmo burbuja.
+	 *
+	 * @return the integer[]
+	 */
+	private static Integer[] algoritmoBurbuja() { //Esta funcion ordena el arreglo 
+		
+		int apuntadorDos = 1;
+		int auxiliar = 0;
+		
+		for(int i = 0; i < arregloV.length; i++) {
+			
+			for(int apuntadorUno = 0; apuntadorUno < arregloV.length; apuntadorUno++) {
+				
+				if(apuntadorDos < arregloV.length) {
+					
+					if(arregloV[apuntadorUno] > arregloV[apuntadorDos]) {
+						
+						auxiliar = arregloV[apuntadorUno];
+						
+						arregloV[apuntadorUno] = arregloV[apuntadorDos];
+						
+						arregloV[apuntadorDos] = auxiliar;
+					}
+					
+					apuntadorDos++;
+				}
+					
+			}
+			apuntadorDos = 1;
+		}
+		return arregloV;
+	}
+	
+	/**
+	 * Busqueda binaria.
+	 *
+	 * @return the integer
+	 * @throws BusquedaException the busqueda exception
+	 */
+	private static Integer busquedaBinaria() throws BusquedaException{
+		Integer[] arregloV=algoritmoBurbuja();
+		if(valorBuscado>100) {														//si el numero que entra por parametro es mayor al limite de los valores del array entonces se sale
+			throw new BusquedaException("Lo sentimos, el numero es mayor al permitido");
+		}
+		
+		Integer apuntadorInicio= new Integer(0);
+		Integer apuntadorFinal= new Integer(arregloV.length);
+		Integer apuntadorPuntoMedio= new Integer(0);
+		Integer valorEncontrado= -1;													//le doy un valor negativo porque no existe la posicion del array -1 y así puedo saber si no se ha encontrado el valor
 		
 		if(valorBuscado>100) {														//si el numero que entra por parametro es mayor al limite de los valores del array entonces se sale
 			throw new BusquedaException("Lo sentimos, el numero es mayor al permitido");
@@ -59,19 +103,19 @@ public class BusquedaBinaria {
 
 		for(int i = 0; i < arregloV.length; i++) {										//el for nos permitirá dar tantas vueltas como sean necesarias hasta encontrar el valor
 
-			apuntadorPM= (apuntador1+apuntador2)/2;										//aquí posicionamos el apuntador PuntoMedio en su posición correspondiente con cada vuelta del bucle
+			apuntadorPuntoMedio= (apuntadorInicio+apuntadorFinal)/2;										//aquí posicionamos el apuntador PuntoMedio en su posición correspondiente con cada vuelta del bucle
 																						//el apuntador puntomedio es el que descubre el valor y los otros dos lo ayudan a llegar 
-			if(valorBuscado == arregloV[apuntadorPM]) {									//cuando el apuntadorPM encuentra el valor buscado entonces alojamos la posicion el el valorEncontrado que será el que retornará
-				valorEncontrado = apuntadorPM;
+			if(valorBuscado == arregloV[apuntadorPuntoMedio]) {									//cuando el apuntadorPM encuentra el valor buscado entonces alojamos la posicion el el valorEncontrado que será el que retornará
+				valorEncontrado = apuntadorPuntoMedio;
 			} else 
-				if(valorBuscado > arregloV[apuntadorPM]){								//cuando el valorBuscado sea mayor al valor que está en la posicion de apuntador el apuntador1 se moverá hasta la posicion del apuntadorPM + 1
-					apuntador1= apuntadorPM++;
+				if(valorBuscado > arregloV[apuntadorPuntoMedio]){								//cuando el valorBuscado sea mayor al valor que está en la posicion de apuntador el apuntador1 se moverá hasta la posicion del apuntadorPM + 1
+					apuntadorInicio= apuntadorPuntoMedio++;
 				} else 
-					if(valorBuscado < arregloV[apuntadorPM]) {							//cuando el valorBuscado sea menor al valor que está en la posicion de apuntador el apuntador2 se moverá hasta la posicion del apuntadorPM - 1
-						apuntador2= apuntadorPM--;
+					if(valorBuscado < arregloV[apuntadorPuntoMedio]) {							//cuando el valorBuscado sea menor al valor que está en la posicion de apuntador el apuntador2 se moverá hasta la posicion del apuntadorPM - 1
+						apuntadorFinal= apuntadorPuntoMedio--;
 					} 
 
-			if(apuntadorPM < 0) {															//es posible que despues de la operacion (apuntador1+apuntador2)/2 el apuntadorPM tenga un valor menor a 0 y dará un error al tratar de acceder al array en una posición que no existe 
+			if(apuntadorPuntoMedio < 0) {															//es posible que despues de la operacion (apuntador1+apuntador2)/2 el apuntadorPM tenga un valor menor a 0 y dará un error al tratar de acceder al array en una posición que no existe 
 				throw new BusquedaException("Lo sentimos, el numero no fue encontrado");
 			}
 		}
@@ -83,12 +127,57 @@ public class BusquedaBinaria {
 		return valorEncontrado;
 	}
 
+	
+
+	/**
+	 * Imprimir datos busqueda binaria.
+	 *
+	 * @throws BusquedaException the busqueda exception
+	 */
+	public void imprimirDatosBusquedaBinaria() throws BusquedaException {
+		
+		System.out.println("***********************************************************");
+		System.out.println("*****Bienvenido al programa de ordenamiento y busqueda*****");
+		System.out.println("***********************************************************");
+		System.out.println("");
+		System.out.println("El valor buscado es: " + valorBuscado);
+		System.out.println("");
+		
+		System.out.println("Los numeros desordenados son: ");
+		Integer[] arregloDesordenado=(arregloOriginal());
+		for(Integer indice = new Integer(0); indice < arregloDesordenado.length; indice++) {
+			
+			System.out.print("[" + arregloDesordenado[indice] + "]");
+		}
+		System.out.println("");
+		System.out.println("Los numeros ordenados son: ");
+		Integer[] arregloOrdenado= algoritmoBurbuja();
+		for(Integer indice = new Integer(0); indice < arregloOrdenado.length; indice++) {
+			
+			System.out.print("[" + arregloOrdenado[indice] + "]");
+		}
+		
+		System.out.println("");
+		System.out.println("");
+		
+		if(busquedaBinaria()<0) {
+			System.out.println("El valor no fue encontrado");
+		}else {
+			System.out.println("Valor encontrado en la posición:" + busquedaBinaria());
+		}
+		
+	}
+
+	/**
+	 * The main method.
+	 *
+	 * @param args the arguments
+	 */
 	public static void main(String[] args) {
 		
-		ordenamientoBurbuja();
-
 		try{
-			System.out.println("¡Enhorabuena, valor encontrado en la posición " + (busquedaBinaria(30)+1) + "!");;
+			BusquedaBinaria test= new BusquedaBinaria(30);
+			test.imprimirDatosBusquedaBinaria();
 		}
 		catch (BusquedaException e){
 
@@ -96,5 +185,7 @@ public class BusquedaBinaria {
 			System.out.println(e.getMessage());
 		}
 	}
+	
+
 
 }
